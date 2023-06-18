d1213b4914112eb29f8577353d3e38ac492896bd
jihai <jihaiwork@gmail.com>
2023-06-18 11:01:59 +0800

    sql ddl

---
+++ src/main/resources/ddl.sql
@@ -0,0 +1,125 @@
+CREATE TABLE `config` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `key` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `deleted` int NOT NULL DEFAULT '0',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_key` (`key`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `file` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) DEFAULT NULL,
+  `user_id` bigint NOT NULL,
+  `url` text,
+  `type` int DEFAULT NULL COMMENT '1-视频，2-图片，3-音频',
+  `format` varchar(16) DEFAULT NULL,
+  `deleted` tinyint NOT NULL DEFAULT '0',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `operate_log` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `type` int NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx_user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `post` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '' COMMENT '标题',
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '内容',
+  `creator_id` bigint NOT NULL COMMENT '创建者id',
+  `topic_id` int DEFAULT NULL COMMENT '话题id',
+  `last_updater_id` bigint DEFAULT NULL COMMENT '最近回复人',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx_update_time` (`title`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `reply` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `send_user_id` bigint NOT NULL,
+  `receiver_id` bigint NOT NULL,
+  `post_id` bigint NOT NULL,
+  `target_reply_id` bigint DEFAULT NULL,
+  `reply_content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `deleted` int NOT NULL DEFAULT '0',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idx_post_id_receiver_id` (`post_id`,`receiver_id`),
+  KEY `idx_post_id_send_user_id` (`post_id`,`send_user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `reply_notice` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `post_id` bigint NOT NULL,
+  `reply_id` bigint NOT NULL,
+  `notify_user_id` bigint NOT NULL,
+  `status` int NOT NULL DEFAULT '0' COMMENT '0-未读，1-已读',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `idx_notify_user_id` (`post_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `topic` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `deleted` int NOT NULL DEFAULT '0',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `user` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '花名',
+  `avatar` varchar(256) DEFAULT NULL,
+  `position` varchar(128) DEFAULT NULL,
+  `city` varchar(64) DEFAULT NULL,
+  `seniority` int DEFAULT NULL COMMENT '工龄',
+  `token` varchar(128) DEFAULT NULL,
+  `level` int DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  `email` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `password` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `contact` varchar(128) DEFAULT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `deleted` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_email` (`email`),
+  UNIQUE KEY `unique_name` (`name`),
+  KEY `idx_token` (`token`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- 字典初始化数据
+
+INSERT INTO `config` (`id`, `key`, `value`, `deleted`, `create_time`, `update_time`)
+VALUES
+	(1, 'SECRET', 'test-secret', 0, '2023-05-30 16:19:03', '2023-05-30 16:19:03'),
+	(2, 'TOP_POST_ID', '1', 0, '2023-05-30 16:48:38', '2023-06-07 09:51:52'),
+	(3, 'MAIL_SECRET', '{\n    \"secret\":\"test\",\n    \"sendMail\":\"test@qq.com\"\n}', 0, '2023-06-08 11:14:00', '2023-06-08 11:17:54'),
+	(4, 'DEFAULT_PASSWORD', '123456', 0, '2023-06-09 19:40:07', '2023-06-09 19:40:09'),
+	(5, 'DEFAULT_POSTER', 'xxxx', 0, '2023-06-13 15:10:14', '2023-06-13 15:10:14');
