8389397ed2d4dc8bb647f7f7ee8d36d524e87775
jihai <jihaiwork@gmail.com>
2023-07-11 17:06:00 +0800

    修复id必传bug

--- src/main/java/com/jihai/bitfree/dto/req/UserReplyReq.java
+++ src/main/java/com/jihai/bitfree/dto/req/UserReplyReq.java
@@ -1,10 +1,7 @@
 package com.jihai.bitfree.dto.req;
 
-import javax.validation.constraints.NotNull;
-
 public class UserReplyReq extends PageQueryReq {
 
-    @NotNull(message = "id不能为空")
     private Long id;
 
     public Long getId() {
