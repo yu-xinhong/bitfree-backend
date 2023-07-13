bef432e3eded33ea9fdf27c7532c1b8449c10e62
jihai <jihaiwork@gmail.com>
2023-07-13 19:03:30 +0800

    聊天内容审核

--- src/main/java/com/jihai/bitfree/dto/req/SendMessageReq.java
+++ src/main/java/com/jihai/bitfree/dto/req/SendMessageReq.java
@@ -1,11 +1,13 @@
 package com.jihai.bitfree.dto.req;
 
+import com.jihai.bitfree.aspect.SensitiveText;
 import com.jihai.bitfree.base.BaseReq;
 import org.hibernate.validator.constraints.Length;
 
 public class SendMessageReq extends BaseReq {
 
     @Length(max = 400, message = "消息不能太长哦")
+    @SensitiveText
     private String content;
 
     public String getContent() {
