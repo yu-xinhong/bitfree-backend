7beae8eeb647f2af250cf3e18512d237b17cb5c3
Zch996 <zhaochenhang@126.com>
2023-08-10 15:45:37 +0800

    优化文案

--- src/main/java/com/jihai/bitfree/constants/Constants.java
+++ src/main/java/com/jihai/bitfree/constants/Constants.java
@@ -4,7 +4,7 @@ public interface Constants {
     String TOP_POST_ID = "TOP_POST_ID";
     String SECRET = "SECRET";
     String TOKEN = "token";
-    String NOT_LOGIN = "请重新登陆!";
+    String NOT_LOGIN = "请重新登录!";
     String EMAIL_SECRET = "MAIL_SECRET";
     String DEFAULT_PASSWORD_KEY = "DEFAULT_PASSWORD";
     String DEFAULT_POSTER = "DEFAULT_POSTER";
