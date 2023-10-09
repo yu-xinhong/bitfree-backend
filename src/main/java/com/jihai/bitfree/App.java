6c4c676d712b83c0bcf70e16ce20b2eaa0554ee0
jihai <jihaiwork@gmail.com>
2023-10-09 13:04:39 +0800

    每日统计数据重置Job启动

--- src/main/java/com/jihai/bitfree/App.java
+++ src/main/java/com/jihai/bitfree/App.java
@@ -4,9 +4,11 @@ package com.jihai.bitfree;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.jihai.bitfree.dao"})
+@EnableScheduling
 public class App {
 
     public static void main(String[] args) {
