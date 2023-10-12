869d17496b45e59ff515dc1ccf79570e0f849e91
jihai <jihaiwork@gmail.com>
2023-10-12 21:58:22 +0800

    添加事务

--- src/main/java/com/jihai/bitfree/service/StatisticService.java
+++ src/main/java/com/jihai/bitfree/service/StatisticService.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import java.util.Date;
@@ -77,6 +78,7 @@ public class StatisticService {
     }
 
     @Async("statisticThreadPool")
+    @Transactional
     public void recordUserLog(Long userId) {
         // 上层单机单线程不存在并发
         try {
