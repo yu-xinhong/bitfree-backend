d8db9411ce4709b6c04b065df73242bfbd901ced
jihai <jihaiwork@gmail.com>
2023-10-21 02:11:54 +0800

    安全提示优化

--- src/main/java/com/jihai/bitfree/base/enums/ReturnCodeEnum.java
+++ src/main/java/com/jihai/bitfree/base/enums/ReturnCodeEnum.java
@@ -4,6 +4,7 @@ public enum ReturnCodeEnum {
     SUCCESS(200, "成功"),
     SYSTEM_ERROR(500, "系统异常"),
     BUSINESS_ERROR(501, "业务异常"),
+    SECRET_ERROR(502, "密钥错误"),
     USER_OLD_PASSWORD_ERROR(403, "原密码错误"),
     SAME_PASSWORD_ERROR(405, "不能与原密码一样"),
     SEND_MAIL_ERROR(406, "调用邮件服务器错误");
