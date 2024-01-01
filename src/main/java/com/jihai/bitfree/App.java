0c522a33778cb8c80d77d5ffacb9232b61095746
magic-programer <3068629642@qq.com>
2024-01-01 22:40:45 +0800

    feature:🐳🐳增加一个有趣的banner,更有社区气息

--- src/main/java/com/jihai/bitfree/App.java
+++ src/main/java/com/jihai/bitfree/App.java
@@ -2,9 +2,13 @@ package com.jihai.bitfree;
 
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -17,5 +21,11 @@ public class App {
 
     public static void main(String[] args) {
         new SpringApplication(App.class).run(args);
+    }
+
+    @EventListener( ApplicationReadyEvent.class )
+    public void ready() {
+        Logger logger = LoggerFactory.getLogger( App.class );
+        logger.info( "\uD83D\uDE80\uD83D\uDE80\uD83D\uDE80 启动成功，欢迎加入bitfree" );
     }
 }
